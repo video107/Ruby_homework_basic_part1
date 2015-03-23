53529074fe6942fe135d5d8cbfd9404a74b1f42c
John Chang <tschangc@gmail.com>
2015-03-23 17:04:24 +0800

    finish flow_control 5 exercise

---
+++ flow_control/5.rb
@@ -0,0 +1,18 @@
+puts "Please input a integer from 0~100"
+number = gets.chomp.to_i
+
+case
+  when number <= 0
+    puts "your number is smaller or equal to 0"
+  when number <= 50
+    puts "your number is smaller or equal to 50 but larger than 0"
+  when number <= 100
+    puts "your number is smaller or equal to 100 but larger than 50"
+  when number > 100
+    puts "your number is larger than 100"
+  else
+    puts "I do not know what you are typing"
+  end
+
+
+
