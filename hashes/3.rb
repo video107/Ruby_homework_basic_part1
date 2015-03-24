1b9e35c50b96c222c5fdc179fe8fadab7623a579
John Chang <tschangc@gmail.com>
2015-03-24 15:45:33 +0800

    finish hash 3 exercise

---
+++ hashes/3.rb
@@ -0,0 +1,6 @@
+hash1 = {name:"jakie", age:"28", height:"180cm", male:"men", weight:"90kg"}
+
+
+hash1.each {|key,value| puts "These are keys:#{key}"}
+hash1.each {|key,value| puts "These are values:#{value}"}
+hash1.each {|key,value| puts "These are keys:#{key} and values:#{value}"}
